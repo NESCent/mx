c99e2ea5ce90785cca86c9da78f17e32c806d945
Dan Leehr <dan.leehr@nescent.org>
2013-02-25 10:38:16 -0500

    Iniitial model customization

--- app/models/tos_import_dataset.rb
+++ app/models/tos_import_dataset.rb
@@ -1,4 +1,10 @@
 class TosImportDataset < ActiveRecord::Base
   belongs_to :proj
   belongs_to :people
+  
+  attr_accessor :filename
+  attr_reader :status
+  
+  validates_presence_of :filename
+  
 end
