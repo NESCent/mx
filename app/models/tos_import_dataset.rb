a705242209888aa9826e87adb8062e2263162f69
Dan Leehr <dan.leehr@nescent.org>
2013-02-26 09:24:32 -0500

    Functionality in dataset model

--- app/models/tos_import_dataset.rb
+++ app/models/tos_import_dataset.rb
@@ -1,10 +1,40 @@
 class TosImportDataset < ActiveRecord::Base
   belongs_to :proj
-  belongs_to :people
+  belongs_to :people, :class_name => "Person"
   
-  attr_accessor :filename
+  attr_accessor :uploaded_file
+  attr_protected :filename, :filepath
   attr_reader :status
+  attr_accessible :uploaded_file, :status
+    
+  before_save :handle_uploaded_file
+  after_save :clear_uploaded_file
+  before_destroy :delete_filesystem_file
   
-  validates_presence_of :filename
+  private
+  
+  def handle_uploaded_file
+    unless uploaded_file.blank?
+      self.filename = base_part_of(uploaded_file.original_filename)
+      output_path = Rails.root.join('public','uploads', uploaded_file.original_filename)
+      File.open(output_path, 'wb') do |file|
+        file.write(uploaded_file.read)
+      end
+      self.filepath = output_path.to_s
+    end
+  end
+  
+  def clear_uploaded_file
+    self.uploaded_file = nil
+    return true
+  end
+  
+  def base_part_of(file_name)
+    File.basename(file_name).gsub(/[^\w._-]/,'')
+  end
+
+  def delete_filesystem_file
+      File.delete(filepath)
+  end
   
 end
